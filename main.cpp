a39f18c00b6eb5f4cf9f6f2e6fe990f605e8d920
SSGSSACE <quangbayhai@gmail.com>
2021-07-16 23:47:33 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+int n;
+void Nhapmang(int a[],int n);
+void Xuatmang(int a[],int n);
+int main()
+{
+    int a[MAX];
+
+    std::cout << "Nhap vao so luong phan tu mang: ";
+    cin >> n;
+    Nhapmang(a, n);
+    Xuatmang(a, n);
+
+    return 0;
+}
+void Nhapmang(int a[MAX], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "\nNhap phan tu thu " << i + 1 << ": ";
+        cin >> a[i];
+    }
+}
+void Xuatmang(int a[MAX], int n)
+{
+    std::cout << "Mang sau khi nhap la:" << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << a[i] << "\t";
+    }
+}
