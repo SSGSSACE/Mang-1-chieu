86ef586a0bfe6054401725a8c9332bd887b3d08f
SSGSSACE <quangbayhai@gmail.com>
2021-07-17 02:17:04 +0700

    July 17, 2021 2:17 AM

--- main.cpp
+++ main.cpp
@@ -60,11 +60,9 @@ void timkiem(int a[MAX], int n, int x)
     for (int i = 0; i < n; i++)
     {
         if (a[i] == x)
-        {
+
             std::cout << "Vi tri dang tim kiem la: " << i;
             
-        }
-     nhan:
      break;
     }
 }
